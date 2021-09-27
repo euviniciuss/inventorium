1d4eb20fbb065f0ab3e1c8d2d56eae0f721805e5
dev-viniciuss <dev.mvsc@gmail.com>
2021-09-27 11:14:39 -0300

    feat: adicionando navegação a página de produtos

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
+import { CreateProductComponent } from './pages/create-product/create-product.component';
 
 const routes: Routes = [
   {
     path: "",
     component: HomeComponent
+  },
+  {
+    path: "products",
+    component: CreateProductComponent
   }
 ];
 
