5d16d3b0e0441dcff325ed7d7b5544d9b608d378
marcus.vinicius <vinicius.costa@ceuma.br>
2021-10-02 18:59:56 -0300

    feat: adicionando navegação dos componentes

---
+++ frontend/src/app/pages/product-create/product.model.ts
@@ -0,0 +1,5 @@
+export interface ProductProps {
+  id?: number,
+  name: string,
+  price: number,
+}
--- frontend/src/app/pages/products/products.component.ts
+++ frontend/src/app/pages/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-products',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  navigateToProductsCreate(): void {
+    this.router.navigate(['/product-create']);
   }
 
 }
